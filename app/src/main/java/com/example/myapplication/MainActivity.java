c5cb79d1addb45a22c5c34069b9f7a1712dfe9f7
Aditya Singh <adityasingh246810@gmail.com>
2021-09-07 10:24:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tablayout;
+    private AppBarLayout appBarLayout;
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tablayout=(TabLayout) findViewById(R.id.tablayout_id);
+        appBarLayout = (AppBarLayout) findViewById(R.id.appbarid);
+        viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+        ViewPageAdapter adapter =new ViewPageAdapter(getSupportFragmentManager());
+        //Adding Fragments
+        adapter.AddFragment(new ongoing(),"Ongoing");
+        adapter.AddFragment(new history(),"History");
+        //adapter Setup
+        viewPager.setAdapter(adapter);
+        tablayout.setupWithViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/ongoing.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ongoing extends Fragment {
+    View view;
+    public ongoing() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.ongoing,container,false);
+        return view;
+
+    }
+}
